320a2e8c7932a53668201110ac52c74639c75040
Jon Ramer <jramer87@gmail.com>
2017-10-31 16:02:48 -0500

    Adds real recipe data to recipe-item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Braised Jackrabbit', 'This is the best rabbit you will ever taste', 'http://images.media-allrecipes.com/userphotos/250x250/639021.jpg')
+  ];
 
   constructor() { }
 
