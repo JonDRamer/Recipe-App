dc5b0e9bef9400bf56d1e49ada86a72824bc31f5
Jon Ramer <jramer87@gmail.com>
2017-11-07 16:01:25 -0600

    Abstracts recipe item out to it's own component and binds to the recipe property from the recipe list component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  // Output makes this event available for parent components to listen to
+  // Event emitter makes this property broadcast an event whenever the
+  // onSelect method is called in the child component (header) template
   @Output() featureSelected = new EventEmitter<string>();
 
+  // Gets called whenever a user clicks on a navigation link in the header
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
   }
